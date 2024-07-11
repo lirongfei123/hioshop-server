dbaff7d625c357ab708c9e177101dc0de1fffea9
rongping.lrp <rongping.lrp@gmail.com>
2024-07-11 08:12:43 +0800

    feat: add config

--- src/common/config/database.js
+++ src/common/config/database.js
@@ -1,13 +1,25 @@
 const mysql = require('think-model-mysql');
-
+import * as dotenv from "dotenv";
+let envpath;
+switch (process.env.NODE_ENV) {
+  case "development":
+    envpath = `${__dirname}/../.env.development`;
+    break;
+  case "production":
+    envpath = `${__dirname}/../.env.production`;
+    break;
+  default:
+    envpath = `${__dirname}/../.env.development`;
+}
+dotenv.config({ path: path.resolve(envpath) });
 module.exports = {
     handle: mysql,
     database: 'hiolabsDB',
     prefix: 'hiolabs_',
     encoding: 'utf8mb4',
-    host: '127.0.0.1',
-    port: '3306',
-    user: 'root',
-    password: '123123123',
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
     dateStrings: true
 };
